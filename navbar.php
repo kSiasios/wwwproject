c3f27cf24ac828b854b69fbefeba8cb671a92f63
kSiasios <76401229+kSiasios@users.noreply.github.com>
2021-01-22 13:17:01 +0200

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -43,7 +43,6 @@
                         echo '<li class="nav-item"><a href="/Project2/index.php" class="nav-link">Home</a></li>';
                     }
                     echo '<li class="nav-item"><a href="#" class="nav-link" id="myBtn">About</a></li>';
-                    // echo '<li class="nav-item"><a href="#" class="nav-link" id="myBtn">Contact Us</a></li>';
                     if (isset($_SESSION["useruid"])) {
                         if (strpos($_SERVER['REQUEST_URI'], 'contact.php') !== false) {
                             echo '<li class="nav-item"><a href="/Project2/pages/contact.php?contactID=0" class="nav-link active">Contact</a></li>';
@@ -74,65 +73,7 @@
         </div>
     </nav>
     <?php
-    // echo    '<div id="myModal" class="modal">
 
-    //             <!-- Modal content -->
-    //             <div class="modal-content">
-    //             <span class="close">&times;</span>
-    //             <p style="padding: 5px; margin-left: 15px; bottom: 0;">Call us or send an email!</p>
-    //             <p style="padding: 5px; margin-left: 15px; bottom: 0;">Tel: 555-555-5555</p>
-    //             <form method="post">
-    //                 <label for="email-address" style="padding: 5px; margin-left: 15px;">Our Email Address:</label>
-    //                 <input type="text" id="email-address" name="email-address" value="email@myemail.com" readonly
-    //                 style="border: none; border-radius: 3px; width: 20vw; max-width: 1000px; padding: 5px; margin: 5px; bottom: 0;"><br>
-
-    //                 <label for="user-email-address" style="padding: 5px; margin-left: 15px;">Your Email Address:</label>';
-
-    // if (isset($_SESSION["useremail"])) {
-    //     echo '<input type="text" id="user-email-address" name="user-email-address" value="' . $_SESSION["useremail"] . '"
-    //                 style="border: none; border-radius: 3px; width: 20vw; max-width: 1000px; padding: 5px; margin: 5px; bottom: 0;"><br>';
-    // } else {
-    //     echo '<input type="text" id="user-email-address" name="user-email-address" placeholder="email@myemail.com"
-    //                 style="border: none; border-radius: 3px; width: 20vw; max-width: 1000px; padding: 5px; margin: 5px; bottom: 0;"><br>';
-    // }
-    // echo '          <textarea type="text" name="email-content" id="email-content" cols="80" rows="2"
-    //                     placeholder="Your Message..."
-    //                     style="border: none; border-radius: 3px; width: 70vw; max-width: 1000px; padding: 5px; margin-left: 15px; bottom: 0;"></textarea>
-
-    //                 <input class="login-btn" style="margin: 15px;" type="button" value="Send Email" 
-    //                     onclick="sendEmail()" /> 
-    //             </form>
-    //             </div>
-
-    //         </div>
-    //         <script>
-    //             // Get the modal
-    //             var modal = document.getElementById("myModal");
-
-    //             // Get the button that opens the modal
-    //             var btn = document.getElementById("myBtn");
-
-    //             // Get the <span> element that closes the modal
-    //             var span = document.getElementsByClassName("close")[0];
-
-    //             // When the user clicks on the button, open the modal
-    //             btn.onclick = function() {
-    //             modal.style.display = "block";
-    //             }
-
-    //             // When the user clicks on <span> (x), close the modal
-    //             span.onclick = function() {
-    //             modal.style.display = "none";
-    //             }
-
-    //             // When the user clicks anywhere outside of the modal, close it
-    //             window.onclick = function(event) {
-    //                 if (event.target == modal) {
-    //                     modal.style.display = "none";
-    //                 }
-    //             }
-    //         </script>';
-    // 
     echo    '<div id="myModal" class="modal">
 
         <!-- Modal content -->
